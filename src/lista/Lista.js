b7cd11c3a1714b6e9382dd99a613bd5e3e82f4b5
Douglas Alves <douglas.expalves@gmail.com>
2017-11-12 12:23:20 -0200

    new example post for the list

--- src/lista/Lista.js
+++ src/lista/Lista.js
@@ -16,10 +16,28 @@ class Lista extends Component {
         },
         {
           id: 3,
-          title: 'ReactIntro is Awesome'
+          title: 'ReactIntro is Awesome!'
+        },
+        {
+          id: 4,
+          title: 'Medium é massa!'
+        },
+        {
+          id: 5,
+          title: 'Acabou minha criatividade!'
         }
       ]
     }
+
+    setTimeout(() => {
+      this.setState({
+        posts: [
+          ...this.state.posts,
+          { id: 6, title: 'Novo post no fim da lista' }
+        ]
+      })
+    }, 3000)
+
     this.render.bind(this)
   }
   render () {
