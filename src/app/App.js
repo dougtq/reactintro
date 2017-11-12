6a81b828ee4b523f2a69afbc7f65b68fa06e03fc
Douglas Alves <douglas.expalves@gmail.com>
2017-11-12 12:22:56 -0200

    updated App.js

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,8 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Bem Vindo to ReactIntro</h1>
+          <h1 className='App-title'>Bem Vindo ao ReactIntro</h1>
+          <hr />
         </header>
       </div>
     )
