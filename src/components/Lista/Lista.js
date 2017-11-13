fbde7aee998ed9b84a351159f1e1b71b1edff8de
Douglas Alves <douglas.expalves@gmail.com>
2017-11-13 01:52:34 -0200

    new Add post feature

--- src/components/Lista/Lista.js
+++ src/components/Lista/Lista.js
@@ -5,38 +5,56 @@ class Lista extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      posts: [
-        {
-          id: 1,
-          title: 'RocketSeat é massa!'
-        },
-        {
-          id: 2,
-          title: 'ReactIntro tbm é massa!'
-        }
-      ]
+      posts: [],
+      newPost: ''
     }
 
-    setTimeout(() => {
-      this.setState({
-        posts: [
-          ...this.state.posts,
-          {
-            id: 3,
-            title: 'Medium é 10/10'
-          }
-        ]
-      })
-    }, 3000)
+    // setTimeout(() => {
+    //   this.setState({
+    //     posts: [
+    //       ...this.state.posts,
+    //       {
+    //         id: 3,
+    //         title: 'Medium é 10/10'
+    //       }
+    //     ]
+    //   })
+    // }, 3000)
 
-    this.render.bind(this)
+    this.render = this.render.bind(this)
+    this.handleNewPost = this.handleNewPost.bind(this)
+    this.createPost = this.createPost.bind(this)
   }
+
+  createPost (e) {
+    let arr = this.state.posts
+    let cont = arr.length + 1
+
+    this.setState({
+      posts: [
+        ...this.state.posts,
+        {
+          id: cont,
+          title: this.state.newPost
+        }
+      ]
+    })
+    this.setState({ newPost: '' })
+    e.preventDefault()
+  }
+
+  handleNewPost (e) {
+    this.setState({ newPost: e.target.value })
+  }
+
   render () {
     return (
       <div className='list'>
         {this.state.posts.map(post =>
           <Post key={post.id} title={post.title} />
         )}
+        <input value={this.state.newPost} onChange={this.handleNewPost} />
+        <button className='btn btn-success' type='submit' onClick={this.createPost}> Criar um novo post</button>
       </div>
     )
   }
